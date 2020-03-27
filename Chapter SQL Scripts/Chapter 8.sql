92f3c3e8f8fb4756589ca8fb2fe2b720d8103f03
msalamone11 <34289629+msalamone11@users.noreply.github.com>
2020-03-27 07:36:21 -0400

    Completed chapter 8 and added chapter 9.

--- Chapter SQL Scripts/Chapter 8.sql
+++ Chapter SQL Scripts/Chapter 8.sql
@@ -1,6 +1,13 @@
-
+SELECT order_num, order_date
+FROM Orders
+WHERE DATEPART(yy, order_date) = 2020 AND DATEPART(mm, order_date) = 1
+ORDER BY order_date
 
 /*
+
+SELECT cust_id, cust_name,
+	   LEFT(cust_contact, 2) + LEFT(cust_city, 3) AS user_login
+FROM Customers
 
 SELECT order_num
 FROM Orders
